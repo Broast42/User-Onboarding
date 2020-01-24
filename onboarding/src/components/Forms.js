594c6a9356b824f972ca3197b179b22586f19227
rdowd <rjarrettdowd@yahoo.com>
2020-01-23 19:34:50 -0500

    Added validation and error messaging to the form.

--- onboarding/src/components/Forms.js
+++ onboarding/src/components/Forms.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { withFormik, Form, Field } from 'formik';
-// import * as Yup from 'yup';
+import { withFormik, Form, Field } from 'formik'; 
+import * as Yup from 'yup';
 // import axios from 'axios';
 import styled from 'styled-components';
 
@@ -11,17 +11,28 @@ const FlexDiv = styled.div`
     width: 220px;
 `;
 
-const FormFields = () => {
+const ErrP = styled.p`
+    color: red;
+`;
+
+const FormFields = ({ errors, touched, values, status }) => {
     return(
         <Form>
             <FlexDiv>
                 <Field type="text" name="name" placeholder="Name" />
+                {touched.name && errors.name && <ErrP>{errors.name}</ErrP>}
+                
                 <Field type="email" name="email" placeholder="Email" />
+                {touched.email && errors.email && <ErrP>{errors.email}</ErrP>}
+
                 <Field type="password" name="password" placeholder="Password" />
+                {touched.password && errors.password && <ErrP>{errors.password}</ErrP>}
+
                 <label>
                     Accept the Terms of Service: 
                     <Field type="checkbox" name="tos" />
                 </label>
+                {touched.tos && errors.tos && <ErrP>{errors.tos}</ErrP>}
                 
                 <button type="submit">Submit</button>
             </FlexDiv>    
@@ -31,8 +42,29 @@ const FormFields = () => {
 
 const Forms = withFormik({
 
+    mapPropsToValues({ name, email, password, tos}){
+        return{
+            name: name || "",
+            email: email || "",
+            password: password || "",
+            tos: tos || false,
+        };
+    },
+    
+    validationSchema: Yup.object().shape({
+        name: Yup.string().required("This field is required"),
+        email: Yup.string().email("Email not valid").required("This field is required"),
+        password: Yup.string().min(8, "Password must be 8 characters or longer").required("This field is required"),
+        tos: Yup.boolean().oneOf([true],"You must accept the terms of service."),
+    }),
+
+    handleSubmit(values){
+        console.log(values);
+    }
 
 })(FormFields);
+
+
 
 export default Forms;
 
